4ee2001ef64e0216aedec40e6792223d06a8a760
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-05-31 20:58:50 +0200

    Update U6.java

--- U6.java
+++ U6.java
@@ -1,5 +1,6 @@
 package qiaw99;
 import java.util.Random;
+import java.util.Scanner;
 
 class Circle{
 	private double x;
@@ -43,24 +44,136 @@ class Circle{
 		}
 	}
 	public static Rectangle smallestBoundingRechtangle(Circle [] circles){
-		return new Rectangle(0,0,1,1);
+		Circle temp=circles[0];
+		for(Circle c : circles){
+			if(!temp.contains(c)){
+				temp=c;
+			}
+		}
+		return new Rectangle(temp.x-temp.radius,temp.y-temp.radius,2*temp.radius,2*temp.radius);
 	}
 }
 
 class Rectangle{
-	private int x;
-	private int y;
-	private int height;
-	private int width;
-	public Rectangle(int x,int y,int width,int height){
-		
+	private double x;
+	private double y;
+	private double height;
+	private double width;
+	public Rectangle(double x,double y,double width,double height){
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+	}
+	public String getInfo(){
+		return "("+this.x+","+this.y+") with width "+this.width+" and height "+this.height;
+	}
+}
+//Aufgabe 6
+class ZInteger<A,B>{
+	public final int first;		// a)
+	public final int second;	//unmutable
+	public ZInteger(Integer a,Integer b){	//e)
+		this.first=a;
+		this.second=b;
+	}
+	public ZInteger add(ZInteger z){	//b)
+		return new ZInteger <A,B> (z.first+this.first,z.second+this.second).simplify();
+	}
+	public ZInteger sub(ZInteger z){
+		return new ZInteger <A,B> (this.first-z.first,this.second-z.second).simplify();
+	}
+	public ZInteger mul(ZInteger z){
+		return new ZInteger <A,B> (this.second*z.first+this.first*z.second,this.second*z.second+this.first*z.first).simplify(); 
+	}
+	public ZInteger simplify(){			//c)
+		int a=this.first;
+		int b=this.second;
+		if(a>=0){
+			while(a>0){
+				a-=1;
+				b+=1;
+			}
+		}else{
+			while(a<0){
+				a+=1;
+				b-=1;
+			}
+		}
+		while(a>0){
+			a-=1;
+			b+=1;
+		}
+		return new ZInteger(a,b);
+	}
+	public boolean equal(ZInteger z){		//d)
+		if(this.first+this.second==z.first+z.second){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public boolean smaller(ZInteger z){
+		if(this.second-this.first<z.second-z.first){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public boolean smallerOrEqual(ZInteger z){
+		if(this.smaller(z) || this.equal(z)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public boolean bigger(ZInteger z){
+		if(this.second-this.first>z.second-z.first){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public boolean biggerOrEqual(ZInteger z){
+		if(this.equal(z) || this.bigger(z)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public String toString(){	//f)
+		return "("+this.first+","+this.second+")";
 	}
 }
 
-public class U6{
+class TestZInteger{		//g)
+	public TestZInteger(){}
+	public static void test(){
+		System.out.println("Test von (1,2) und (2,3)");
+		ZInteger z1=new ZInteger(1,2);
+		ZInteger z2=new ZInteger(2,3);
+		System.out.println("Addition:");
+		System.out.println(z1.add(z2).toString());
+		System.out.println("Substraktion:");
+		System.out.println(z1.sub(z2).toString());
+		System.out.println("Multiplikation:");
+		System.out.println(z1.mul(z2).toString());
+	}
+}
+
+public class test{
 	public static void main(String args[]){
 		int i=(int)(new Random().nextInt(3));
-		System.out.println(mul(45,33));
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.println("*********Aufgabe 1*********");		
+		System.out.println("Wie ist der Multiplikator?");
+		int n=scan.nextInt();
+		System.out.println("Wie ist der Multiplikand?");
+		int m=scan.nextInt();
+		System.out.println("Das Ergebnis: "+mul(n,m));
+		System.out.println("*********Aufgabe 6*********");
+		TestZInteger.test();		
 	}
 	public static int mul(int a, int b){	//Aufgabe 1
 		//Die Komplexitaet ist: log2(n)
