169b7632e9b69596e471664378d0aeb4f7f093f5
qiaw99 <46455899+qiaw99@users.noreply.github.com>
2019-06-04 20:12:18 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-//Übungsgruppe:Qianli und Nazar
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -6,36 +5,33 @@
 #include <string.h>
 
 #define FileName "test.txt"
-#define MAX_SIZE 256
 
 int main() {
-	int fd;
-	char buffer[MAX_SIZE];
-	printf("Input?\n");
-	fgets(buffer,MAX_SIZE-1,stdin);
-
 	struct flock lock;
 	lock.l_type = F_WRLCK;   
 	lock.l_whence = SEEK_SET; 
 	lock.l_start = 0;         
-	lock.l_len = 0;          
+	lock.l_len = 0;           
 	lock.l_pid = getpid();   
-	if ((fd = open(FileName, O_RDWR | O_CREAT, 0666)) ==-1){  
+
+	int fd; 
+	if ((fd = open(FileName, O_RDONLY)) < 0)  
 		fprintf(stderr,"Can't be opened!\n");
-		exit(-1);
-	}
-	if (fcntl(fd, F_SETLK, &lock) < 0){ 
-		fprintf(stderr,"Can't not be locked!\n");
-		exit(-1);
-	}else {
-		write(fd,buffer, strlen(buffer));
-		fprintf(stdout, "Process %d\n", lock.l_pid);
-	}
+
+	fcntl(fd, F_GETLK, &lock);
+	if (lock.l_type != F_UNLCK)
+		fprintf(stderr,"Write locked!\n");
+
+	lock.l_type = F_RDLCK; 
+	if (fcntl(fd, F_SETLK, &lock) < 0)
+		fprintf(stderr,"Can't get a read-only lock!\n");
+
+	int c; 
+	while (read(fd, &c, 1) > 0)    
+		write(STDOUT_FILENO, &c, 1); 
 	lock.l_type = F_UNLCK;
-	if (fcntl(fd, F_SETLK, &lock) < 0){
+	if (fcntl(fd, F_SETLK, &lock) < 0)
 		fprintf(stderr,"Can't be unlocked!\n");
-		exit(-1);
-	}
-	close(fd); 
-	return 0; 
+	close(fd);
+	return 0;
 }
